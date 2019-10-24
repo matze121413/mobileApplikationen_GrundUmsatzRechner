c8575b55643085665aa1884d5d16aae4589b2e47
Matthias Schultze-Westrum <matze.schuwe@gmail.com>
2019-10-24 14:45:02 +0200

    Buttons für Speichern und Neu starten

--- app/src/main/java/matze/schuwe/grundumsatzrechner/ErgebnisActivity.java
+++ app/src/main/java/matze/schuwe/grundumsatzrechner/ErgebnisActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class ErgebnisActivity extends AppCompatActivity {
@@ -15,16 +16,22 @@ public class ErgebnisActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ergebnis);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        db= AktivitaetActivity.db;
+        db = AktivitaetActivity.db;
         TextView grundUmsatzKcal = (TextView) findViewById(R.id.grundUmsatz_kcal);
         TextView grundUmsatzKj = (TextView) findViewById(R.id.grundUmsatz_kj);
         TextView kalorienVerbrauchKcal = (TextView) findViewById(R.id.kalorienVerbrauch_kcal);
         TextView kalorienVerbrauchKj = (TextView) findViewById(R.id.kalorienVerbrauch_kj);
-        grundUmsatzKcal.setText(((int)db.getGrundUmsatz())+" kcal");
-        grundUmsatzKj.setText(((int)(4.186*db.getGrundUmsatz()))+" kj");
-        kalorienVerbrauchKcal.setText(((int)db.getKalorienVerbrauch())+" kcal");
-        kalorienVerbrauchKj.setText(((int)(4.186*db.getKalorienVerbrauch()))+" kj");
+        grundUmsatzKcal.setText(((int) db.getGrundUmsatz()) + " kcal");
+        grundUmsatzKj.setText(((int) (4.186 * db.getGrundUmsatz())) + " kj");
+        kalorienVerbrauchKcal.setText(((int) db.getKalorienVerbrauch()) + " kcal");
+        kalorienVerbrauchKj.setText(((int) (4.186 * db.getKalorienVerbrauch())) + " kj");
 
+    }
+    public void neuStarten(View v){
+        MainActivity.db=null;
+        AktivitaetActivity.db=null;
+        Intent intent = new Intent(ErgebnisActivity.this, MainActivity.class);
+        startActivity(intent);
     }
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
