385ecced23d81ec7f5da96e50b567475136590aa
Franziska <franzivoegele@web.de>
2019-10-26 19:13:54 +0200

    Hilfe_Activity und dazugehörige xml-Datei angelegt.

--- app/src/main/java/matze/schuwe/grundumsatzrechner/ListeDatenbank.java
+++ app/src/main/java/matze/schuwe/grundumsatzrechner/ListeDatenbank.java
@@ -2,8 +2,12 @@ package matze.schuwe.grundumsatzrechner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -32,4 +36,26 @@ public class ListeDatenbank extends AppCompatActivity {
         ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listeDaten);
         liste.setAdapter(adapter);
     }
+
+    /* Menu-Icon wird in toolbar angezeigt */
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.datenpage, menu);
+        return true;
+    }//Ende der onCreateOptionMenu-Methode
+
+    //soll Activity wechseln, wenn ein Item im MEnu angeklickt wurde
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.datenberechnung:
+                Intent berechnungIntent = new Intent(ListeDatenbank.this,MainActivity.class);
+                startActivity(berechnungIntent);
+                return true;
+            case R.id.datenhelp:
+                Intent  helpIntent = new Intent(ListeDatenbank.this,HilfeActivity.class);
+                startActivity(helpIntent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }//Ende switch
+    }//Ende onOptionsItemSelected
 }
